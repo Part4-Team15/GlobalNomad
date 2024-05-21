4f5e5a6d23677dbfa2349250e457edfb90880973
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2024-05-21 11:20:08 +0900

    feat : 로그인 데모 페이지 제작

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import LoginForm from '../components/login/LoginForm';
 
-const LoginPage = () => <div>로그인 페이지!</div>;
+const LoginPage = () => (
+  <div>
+    로그인을 진행해주세요
+    <LoginForm />
+  </div>
+);
 
 export default LoginPage;
