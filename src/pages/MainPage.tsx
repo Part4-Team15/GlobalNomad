619bd1baa121668a96d9ab5d028d27a0f142e1a1
seoyeon <ysynop7@naver.com>
2024-05-22 16:16:51 +0900

    Feat: MainBanner 컴포넌트 적용

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import MainBanner from '../components/mainpage/MainBanner';
 
-const MainPage = () => <div>메인 페이지!</div>;
+const MainPage = () => {
+  const filterList = ['a', 'b', 'c', 'd'];
+  return (
+    <>
+      <MainBanner />
+      <div>search</div>
+      <div>인기 체험🔥</div>
+      <div>cardList</div>
+      <div>
+        {filterList.map((folder) => <div key={folder}>{folder}</div>)}
+      </div>
+      <div>🛼 모든 체험</div>
+      <div>cardList</div>
+      <div>pagination</div>
+    </>
+  );
+};
 
 export default MainPage;
