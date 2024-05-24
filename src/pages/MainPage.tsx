207e5286e6e81811779bc1ee67d288aa889a0ed6
seoyeon <ysynop7@naver.com>
2024-05-24 15:24:35 +0900

    fix: 메인 페이지 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,25 +1,21 @@
+import CategoryFilter from '../components/mainpage/CategoryFilter';
 import ActivityCard from '../components/mainpage/ActivityCard';
 import ActivitySearch from '../components/mainpage/ActivitySearch';
 import MainBanner from '../components/mainpage/MainBanner';
 
-const MainPage = () => {
-  const filterList = ['a', 'b', 'c', 'd'];
-  return (
-    <>
-      <MainBanner />
-      <div className="flex flex-col items-center">
-        <ActivitySearch />
-        <div className="text-4xl font-bold pt-10">🔥인기 체험</div>
-        <ActivityCard />
-        <div>
-          {filterList.map((folder) => <div key={folder}>{folder}</div>)}
-        </div>
-        <div>🛼 모든 체험</div>
-        <div>cardList</div>
-        <div>pagination</div>
-      </div>
-    </>
-  );
-};
+const MainPage = () => (
+  <>
+    <MainBanner />
+    <div className="flex flex-col items-center">
+      <ActivitySearch />
+      <div className="text-4xl font-bold pt-10">🔥인기 체험</div>
+      <ActivityCard />
+      <CategoryFilter />
+      <div className="text-4xl font-bold">🛼 모든 체험</div>
+      <div>cardList</div>
+      <div>pagination</div>
+    </div>
+  </>
+);
 
 export default MainPage;
