c27a2b8705fbd78663b70afe471711d1d6e74d69
beom <gb3187@naver.com>
2024-05-25 20:53:10 +0900

    feat : 주소 찾기 버튼 추가

--- src/components/assignActivity/AssignAddress.tsx
+++ src/components/assignActivity/AssignAddress.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 const AssignAddress = () => (
   <div className=" flex w-[100%] flex-col items-start gap-4">
-    <span className=" text-black text-2xl font-bold">주소</span>
+    <div className="w-[100%] flex justify-between pr-2">
+      <span className=" text-black text-2xl font-bold">주소</span>
+      <button
+        className="flex px-4 py-2 content-center gap-1 items-center self-stretch rounded bg-black text-white text-sm font-bold"
+        type="button"
+      >
+        주소 찾기
+      </button>
+    </div>
     <div className=" flex pt-2 pr-4 pb-2 pl-4 items-center self-stretch rounded-[4px] border border-gray-60">
       <input
         className="w-[100%] outline-none"
