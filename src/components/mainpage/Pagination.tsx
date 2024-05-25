162c381d419d1b0056bf931c4e2f8f8b6a4a5d53
seoyeon <ysynop7@naver.com>
2024-05-25 16:20:17 +0900

    Feat: Pagination 컴포넌트 UI 구현

--- src/components/mainpage/Pagination.tsx
+++ src/components/mainpage/Pagination.tsx
@@ -1,5 +1,32 @@
 import React from 'react';
 
-const Pagination = () => <div>페이지네이션 컴포넌트</div>;
+interface PaginationProp {
+  count: number;
+}
+
+const Pagination = ({ count }: PaginationProp) => {
+  const pageNumber = new Array(count).fill(0).map((num, i) => num + i);
+  console.log(pageNumber);
+
+  return (
+    <div className="flex gap-[10px]">
+      <button className="flex justify-center items-center w-[55px] h-[55px] border border-green-80 rounded-2xl" type="button">
+        <img src="/assets/bold_arrow_left.svg" alt="arrow-left" />
+      </button>
+      {pageNumber.map((pageNum) => (
+        <button
+          className="w-[55px] h-[55px] text-lg text-green-80 bg-white border border-green-80 rounded-2xl hover:bg-green-80 hover:text-white"
+          type="button"
+          key={pageNum}
+        >
+          {pageNum + 1}
+        </button>
+      ))}
+      <button className="flex justify-center items-center w-[55px] h-[55px] border border-green-80 rounded-2xl" type="button">
+        <img src="/assets/bold_arrow_right.svg" alt="arrow-right" />
+      </button>
+    </div>
+  );
+};
 
 export default Pagination;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,19 +1,22 @@
-import CategoryFilter from '../components/mainpage/CategoryFilter';
-import ActivityCard from '../components/mainpage/ActivityCard';
-import ActivitySearch from '../components/mainpage/ActivitySearch';
-import MainBanner from '../components/mainpage/MainBanner';
+import CategoryFilter from '@/components/mainpage/CategoryFilter';
+import ActivityCard from '@/components/mainpage/ActivityCard';
+import ActivitySearch from '@/components/mainpage/ActivitySearch';
+import MainBanner from '@/components/mainpage/MainBanner';
+import Pagination from '@/components/mainpage/Pagination';
 
 const MainPage = () => (
   <>
     <MainBanner />
     <div className="flex flex-col items-center">
       <ActivitySearch />
-      <div className="text-4xl font-bold pt-10">🔥인기 체험</div>
-      <ActivityCard />
-      <CategoryFilter />
-      <div className="text-4xl font-bold">🛼 모든 체험</div>
-      <div>cardList</div>
-      <div>pagination</div>
+      <div className="w-[1200px] mt-10">
+        <div className="text-4xl font-bold mb-[33px]">🔥인기 체험</div>
+        <ActivityCard />
+        <CategoryFilter />
+        <div className="text-4xl font-bold mb-[33px]">🛼 모든 체험</div>
+        <ActivityCard />
+        <Pagination count={8} />
+      </div>
     </div>
   </>
 );
