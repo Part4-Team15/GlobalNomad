a0d4032d18c174450afeb9c83a02f5fb843db1e2
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2024-05-27 15:37:11 +0900

    style : 헤더 컴포넌트 기본 스타일링

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => (
+  <div className="py-[19px]">
+    <img src="assets/logo_small.svg" alt="logo_small_icon" />
+    <div>
+      <img src="assets/notification_icon.svg" alt="notification_icon" />
+    </div>
+  </div>
+);
+
+export default Header;
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,10 +1,11 @@
 import Footer from '@/components/footer/Footer';
+import Header from '@/components/header/Header';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => (
   <>
-    <div>Header</div>
+    <Header />
     <Outlet />
     <Footer />
   </>
