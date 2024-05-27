ac508a266ff13fe7bf6d7671e63209bb36675e76
beom <gb3187@naver.com>
2024-05-27 12:08:20 +0900

    feat : 날짜 모달 구현

--- src/components/assignActivity/AssignReservationTime.tsx
+++ src/components/assignActivity/AssignReservationTime.tsx
@@ -1,18 +1,28 @@
 import React, { useState } from 'react';
 import StartTimeDropDown from './dropDown/StartTimeDropDown';
 import EndTimeDropDown from './dropDown/EndTimeDropDown';
+import CalendarModal from './modal/CalendarModal';
 
 const AssignReservationTime = () => {
   const [isStartTimeDropDown, setIsStartTimeDropDown] = useState(false);
   const [isEndTimeDropDown, setIsEndTimeDropDown] = useState(false);
+  const [isOpenCalendar, setIsOpenCalendar] = useState(false);
   const [time, setTime] = useState({ startTime: '', endTime: '' });
+  const [selectedDate, setSelectedDate] = useState('');
 
+  // 시작 시간 밑의 드랍다운
   const handleStartTimeDropDown = () => {
     setIsStartTimeDropDown(!isStartTimeDropDown);
   };
 
+  // 종료 시간 밑의 드랍다운
   const handleEndTimeDropDown = () => {
     setIsEndTimeDropDown(!isEndTimeDropDown);
+  };
+
+  // 날짜 모달
+  const handleCalendar = () => {
+    setIsOpenCalendar(!isOpenCalendar);
   };
 
   const handleSelectStart = (startTime: string, endTime: string) => {
@@ -25,21 +35,37 @@ const AssignReservationTime = () => {
     setIsEndTimeDropDown(!isEndTimeDropDown);
   };
 
+  const handleDateSelect = (date: string) => {
+    setSelectedDate(date);
+    setIsOpenCalendar(!isOpenCalendar);
+  };
+
   return (
     <div className=" flex w-[100%] flex-col items-start gap-6">
       <span className=" text-black text-2xl font-bold">예약 가능한 시간대</span>
       <div className=" flex w-[100%] flex-col items-start gap-[21px]">
         <div className="flex w-[100%] items-start gap-5">
-          <div className="flex w-[100%] flex-col">
-            <span>날짜</span>
-            <div className=" flex w-[100%] pt-2 pr-4 pb-2 pl-4 items-center self-stretch rounded-[4px] border border-gray-60">
-              <input className="w-[100%] outline-none" placeholder="YY/MM/DD" />
-
-              <img src="/assets/calendar_icon.svg" alt="calendarIcon" />
+          {/* 날짜 */}
+          <div className=" w-[100%] relative">
+            <div className="flex w-[100%] flex-col">
+              <span>날짜</span>
+              <div className=" flex w-[100%] pt-2 pr-4 pb-2 pl-4 items-center self-stretch rounded-[4px] border border-gray-60">
+                <input
+                  className="w-[100%] outline-none"
+                  placeholder="YY/MM/DD"
+                  value={selectedDate}
+                  readOnly
+                />
+                <button type="button" onClick={handleCalendar}>
+                  <img src="/assets/calendar_icon.svg" alt="calendarIcon" />
+                </button>
+              </div>
             </div>
+            {isOpenCalendar && <CalendarModal onSelect={handleDateSelect} />}
           </div>
 
           <div className=" flex h-[70px] w-[100%] items-center gap-3">
+            {/* 시작 시간 */}
             <div className=" w-[100%] relative">
               <div className="flex w-[100%] flex-col ">
                 <span>시작 시간</span>
@@ -68,7 +94,7 @@ const AssignReservationTime = () => {
             </div>
 
             <span className=" mt-4">~</span>
-
+            {/* 종료 시간 */}
             <div className=" w-[100%] relative">
               <div className="flex w-[100%] flex-col ">
                 <span>종료 시간</span>
