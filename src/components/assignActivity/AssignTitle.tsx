71a8652cf1636d0d2a7d3c8e09f050b9d3bd41b5
beom <gb3187@naver.com>
2024-05-29 13:55:30 +0900

    feat : 체험 등록 타입 정의 및 유틸함수 생성

--- src/components/assignActivity/AssignTitle.tsx
+++ src/components/assignActivity/AssignTitle.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+// import { useQueryClient } from '@tanstack/react-query';
+// import { AssignData } from '@/types/assignActivityPage';
 
-const AssignTitle = () => (
-  <div className=" flex pt-2 pr-4 pb-2 pl-4 items-center self-stretch rounded-[4px] border border-gray-60">
-    <input className="w-[100%] outline-none" placeholder="제목" />
-  </div>
-);
+const AssignTitle = () => {
+  // const queryClient = useQueryClient();
+
+  // const handleChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   queryClient.setQueryData<AssignData>(['assignData'], (oldData) => {
+  //     return { ...oldData, title: e.target.value };
+  //   });
+  // };
+
+  return (
+    <div className=" flex pt-2 pr-4 pb-2 pl-4 items-center self-stretch rounded-[4px] border border-gray-60">
+      <input
+        className="w-[100%] outline-none"
+        // onChange={handleChangeTitle}
+        placeholder="제목"
+      />
+    </div>
+  );
+};
 
 export default AssignTitle;
