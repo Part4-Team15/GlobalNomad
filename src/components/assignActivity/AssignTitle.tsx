ecf830d6f211d476bf1a4661455d60b2c0e016d5
beom <gb3187@naver.com>
2024-05-29 14:00:02 +0900

    feat : 제목 - 리액트 쿼리에 저장

--- src/components/assignActivity/AssignTitle.tsx
+++ src/components/assignActivity/AssignTitle.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
-// import { useQueryClient } from '@tanstack/react-query';
-// import { AssignData } from '@/types/assignActivityPage';
+import { useQueryClient } from '@tanstack/react-query';
+import { AssignData } from '@/types/assignActivityPage';
+import mergeAssignData from './utils/mergeAssignData';
 
 const AssignTitle = () => {
-  // const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-  // const handleChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   queryClient.setQueryData<AssignData>(['assignData'], (oldData) => {
-  //     return { ...oldData, title: e.target.value };
-  //   });
-  // };
+  const handleChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    queryClient.setQueryData<AssignData>(['assignData'], (oldData) => {
+      return mergeAssignData(oldData, { title: e.target.value });
+    });
+  };
 
   return (
     <div className=" flex pt-2 pr-4 pb-2 pl-4 items-center self-stretch rounded-[4px] border border-gray-60">
       <input
         className="w-[100%] outline-none"
-        // onChange={handleChangeTitle}
+        onChange={handleChangeTitle}
         placeholder="제목"
       />
     </div>
