289e659aec7b8dd07460d46b70b95032108ad886
seoyeon <ysynop7@naver.com>
2024-05-30 22:40:59 +0900

    fix: 인기 체험 Card 리스트 api 함수 연결

--- src/components/mainpage/PopularActivityList.tsx
+++ src/components/mainpage/PopularActivityList.tsx
@@ -1,13 +1,22 @@
-import { useState } from 'react';
-import { EXAMPLE_ACTIVITY } from '@/lib/utils/activity_mock_data';
+import { useEffect, useState } from 'react';
+import axiosInstance from '@/lib/axiosInstance';
+import { ActivityInfo, ActivityResponse } from '@/types/mainPage';
 import PopularActivityCard from './PopularActivityCard';
 import PopularActivityButton from './PopularActivityButton';
 
 const PopularActivityList = () => {
   const [startIdx, setStartIdx] = useState(0);
-  const { activities } = EXAMPLE_ACTIVITY;
+  const [activity, setActivity] = useState<ActivityInfo[]>([]);
 
-  const pageActivityList = activities.slice(startIdx, startIdx + 3);
+  // 인기 체험 리스트 데이터를 불러오는 함수.
+  async function getPopularActivity() {
+    const res = await axiosInstance.get<ActivityResponse>(
+      '/activities?method=offset&sort=most_reviewed&page=1&size=10'
+    );
+    return res.data;
+  }
+
+  const pageActivityList = activity.slice(startIdx, startIdx + 3);
 
   const handleLeftClick = () => {
     if (startIdx === 0) return;
@@ -18,6 +27,14 @@ const PopularActivityList = () => {
     setStartIdx(startIdx + 1);
   };
 
+  useEffect(() => {
+    const fetchActivity = async () => {
+      const data = await getPopularActivity();
+      setActivity(data.activities);
+    };
+    fetchActivity();
+  }, []);
+
   return (
     <div className="mt-10 mb-[60px]">
       <div className="flex justify-between">
@@ -25,8 +42,8 @@ const PopularActivityList = () => {
         <PopularActivityButton onLeftClick={handleLeftClick} onRightClick={handleRightClick} />
       </div>
       <div className="grid grid-cols-3 gap-6 w-pc">
-        {pageActivityList.map((activity) => (
-          <PopularActivityCard key={activity.id} cardData={activity} />
+        {pageActivityList.map((info) => (
+          <PopularActivityCard key={info.id} cardData={info} />
         ))}
       </div>
     </div>
