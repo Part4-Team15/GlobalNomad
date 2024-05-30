9a863ce19920ef0e56cf496022f8959a40aa8a97
seoyeon <ysynop7@naver.com>
2024-05-30 17:17:26 +0900

    feat: 인기 체험 페이지네이션 컴포넌트 작성

--- src/components/mainpage/PopularActivityList.tsx
+++ src/components/mainpage/PopularActivityList.tsx
@@ -1,20 +1,31 @@
+import { useState } from 'react';
 import { EXAMPLE_ACTIVITY } from '@/lib/utils/activity_mock_data';
 import PopularActivityCard from './PopularActivityCard';
+import PopularActivityButton from './PopularActivityButton';
 
 const PopularActivityList = () => {
+  const [startIdx, setStartIdx] = useState(0);
   const { activities } = EXAMPLE_ACTIVITY;
+
+  const pageActivityList = activities.slice(startIdx, startIdx + 3);
+
+  const handleLeftClick = () => {
+    if (startIdx === 0) return;
+    setStartIdx(startIdx - 1);
+  };
+  const handleRightClick = () => {
+    if (pageActivityList.length < 3) return;
+    setStartIdx(startIdx + 1);
+  };
 
   return (
     <div className="mt-10 mb-[60px]">
       <div className="flex justify-between">
         <div className="text-4xl font-bold mb-[33px]">🔥인기 체험</div>
-        <div>
-          <button type="button">left</button>
-          <button type="button">right</button>
-        </div>
+        <PopularActivityButton onLeftClick={handleLeftClick} onRightClick={handleRightClick} />
       </div>
       <div className="grid grid-cols-3 gap-6 w-pc">
-        {activities.map((activity) => (
+        {pageActivityList.map((activity) => (
           <PopularActivityCard key={activity.id} cardData={activity} />
         ))}
       </div>
