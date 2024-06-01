3a820e34eca286f6dab1ee78a46afb652a0f9708
seoyeon <ysynop7@naver.com>
2024-06-01 12:47:55 +0900

    refactor: api 함수 수정에 따라 불필요한 코드 제거 및 수정

--- src/components/mainpage/ActivityCardList.tsx
+++ src/components/mainpage/ActivityCardList.tsx
@@ -1,8 +1,7 @@
 import Pagination from '@/components/mainpage/Pagination';
 import ActivityCard from '@/components/mainpage/ActivityCard';
 import { useEffect, useState } from 'react';
-import axiosInstance from '@/lib/axiosInstance';
-import { ActivityInfo, ActivityResponse } from '@/types/mainPage';
+import { ActivityInfo } from '@/types/mainPage';
 import getCurrentPageActivity from '@/api/getCurrentPageActivity';
 
 const OFFSET_LIMIT = 8;
@@ -11,16 +10,8 @@ const ActivityCardList = () => {
   const [currenData, setCurrentData] = useState<ActivityInfo[]>([]);
   const [count, setCount] = useState(1);
 
-  // 처음으로 렌더링 시 1페이지 데이터를 불러오는 함수.
-  async function getFirstPageActivity() {
-    const res = await axiosInstance.get<ActivityResponse>(
-      `/activities?method=offset&page=1&size=${OFFSET_LIMIT}`
-    );
-    return res.data;
-  }
-
   // 페이지를 넘길 때마다 해당 페이지의 데이터를 불러오는 함수.
-  const getPageData = async (pageNum: number, size: number) => {
+  const handlePageData = async (pageNum: number, size: number) => {
     try {
       const { activities } = await getCurrentPageActivity(pageNum, size);
       setCurrentData(activities);
@@ -31,7 +22,7 @@ const ActivityCardList = () => {
 
   useEffect(() => {
     const fetchPageData = async () => {
-      const data = await getFirstPageActivity();
+      const data = await getCurrentPageActivity(1, OFFSET_LIMIT);
       setCurrentData(data.activities);
       setCount(data.totalCount);
     };
@@ -41,11 +32,11 @@ const ActivityCardList = () => {
   return (
     <>
       <div className="grid grid-cols-4 gap-6 mb-[72px]">
-        {currenData?.map((activity) => (
+        {currenData.map((activity) => (
           <ActivityCard key={activity.id} cardData={activity} />
         ))}
       </div>
-      <Pagination totalCount={count} offsetLimit={OFFSET_LIMIT} setActivityList={getPageData} />
+      <Pagination totalCount={count} offsetLimit={OFFSET_LIMIT} setActivityList={handlePageData} />
     </>
   );
 };
