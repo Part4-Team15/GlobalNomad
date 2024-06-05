2422bd800acd72c39a5ec2140c4bf4986cb7726c
seoyeon <ysynop7@naver.com>
2024-06-05 19:46:40 +0900

    Refactor: 웹 너비에 따라 보여질 데이터의 개수, offsetLimit 수정

--- src/components/mainpage/ActivityCardList.tsx
+++ src/components/mainpage/ActivityCardList.tsx
@@ -5,13 +5,33 @@ import { ActivityInfo } from '@/types/mainPage';
 import getCurrentPageActivity from '@/api/getCurrentPageActivity';
 import CategoryFilter from './CategoryFilter';
 
-const OFFSET_LIMIT = 8;
+function calculateOffsetLimit() {
+  if (window.innerWidth > 1024) {
+    return 8;
+  }
+  if (window.innerWidth > 769) {
+    return 9;
+  }
+  return 4;
+}
 
 const ActivityCardList = () => {
   const [currenData, setCurrentData] = useState<ActivityInfo[]>([]);
   const [currentCategory, setCurrentCategory] = useState('');
   const [sortActivity, setSortActivity] = useState('');
   const [count, setCount] = useState(1);
+  const [offset, setOffset] = useState(calculateOffsetLimit());
+
+  useEffect(() => {
+    const handleResize = () => {
+      setOffset(calculateOffsetLimit());
+    };
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  });
 
   // 페이지를 넘길 때마다 해당 페이지의 데이터를 불러오는 함수.
   const handlePageData = async (pageNum: number, size: number) => {
@@ -37,12 +57,12 @@ const ActivityCardList = () => {
 
   useEffect(() => {
     const fetchPageData = async () => {
-      const data = await getCurrentPageActivity(0, OFFSET_LIMIT, currentCategory, sortActivity);
+      const data = await getCurrentPageActivity(0, offset, currentCategory, sortActivity);
       setCurrentData(data.activities);
       setCount(data.totalCount);
     };
     fetchPageData();
-  }, [currentCategory, sortActivity]);
+  }, [offset, currentCategory, sortActivity]);
 
   return count ? (
     <>
@@ -57,7 +77,7 @@ const ActivityCardList = () => {
           <ActivityCard key={activity.id} cardData={activity} />
         ))}
       </div>
-      <Pagination totalCount={count} offsetLimit={OFFSET_LIMIT} setActivityList={handlePageData} />
+      <Pagination totalCount={count} offsetLimit={offset} setActivityList={handlePageData} />
     </>
   ) : (
     <div className="flex justify-center items-center">데이터가 없습니다.</div>
