fa2083f2bee92d446d63bbac9bc08f4c1d9d1360
beom <gb3187@naver.com>
2024-06-07 14:11:41 +0900

    feat : 시간 드롭다운 기능 수정

--- src/components/modifyActivity/dropDown/EndTimeDropDown.tsx
+++ src/components/modifyActivity/dropDown/EndTimeDropDown.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
 
 interface EndTimeProps {
   onSelect: (endTime: string) => void;
 }
 
 const EndTimeDropDown = ({ onSelect }: EndTimeProps) => {
+  const { data: reservationStartTime } = useQuery({
+    queryKey: ['modifyData/Schedule/StartTime'],
+  });
   const reservationTime = [
     { endTime: '10:00' },
     { endTime: '11:00' },
@@ -16,13 +20,27 @@ const EndTimeDropDown = ({ onSelect }: EndTimeProps) => {
     { endTime: '17:00' },
   ];
 
+  const parseTime = (time: string) => {
+    const [hours, minutes] = time.split(':').map(Number);
+    const date = new Date();
+    date.setHours(hours, minutes, 0, 0);
+    return date;
+  };
+
+  const startTimeDate = parseTime(reservationStartTime as string);
+
+  const filteredTimes = reservationTime.filter(({ endTime }) => {
+    const endTimeDate = parseTime(endTime);
+    return endTimeDate > startTimeDate;
+  });
+
   const handleSelectedTime = (endTime: string) => () => {
     onSelect(endTime);
   };
 
   return (
     <ul className=" absolute z-10 flex flex-col w-[100%] p-2 items-start gap-[4px] shrink-0 rounded-md bg-white shadow-md">
-      {reservationTime.map((time) => (
+      {filteredTimes.map((time) => (
         <button
           key={time.endTime}
           type="button"
