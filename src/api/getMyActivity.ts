62484db2cc669011a4a2c41054e81eacc20c53ae
Dodam0719 <blueoh719@gmail.com>
2024-06-10 11:26:58 +0900

    fix : 6일 작업분에 8일 재훈님 작업분이 덮어씌워져서 페이지 정상작동하지 않는 문제 수정(6일 작업분 복구)

--- src/api/getMyActivity.ts
+++ src/api/getMyActivity.ts
@@ -1,16 +1,11 @@
 import axiosInstance from '@/lib/axiosInstance';
+
 import { QueryFunctionContext } from '@tanstack/react-query';
 
 const getMyActivity = async ({ queryKey, pageParam }: QueryFunctionContext): Promise<any> => {
-  try {
-    const [, size] = queryKey;
-    const cursorParam = pageParam ? `&cursorId=${pageParam}` : '';
-    const response = await axiosInstance.get(`/my-activities?size=${size}${cursorParam}`);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching My activity data:', error);
-    throw error;
-  }
+  const cursorParam = pageParam ? `cursorId=${pageParam}` : '';
+  const response = await axiosInstance.get(`/my-activities?${cursorParam}&size=5`);
+  return response.data;
 };
 
 export default getMyActivity;
