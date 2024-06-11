91d80419394bcdb6dac276dbad1102c745364f6e
beom <gb3187@naver.com>
2024-06-11 14:30:35 +0900

    feat : input의 color값 변경

--- src/components/assignActivity/AssignTitle.tsx
+++ src/components/assignActivity/AssignTitle.tsx
@@ -13,12 +13,8 @@ const AssignTitle = () => {
   };
 
   return (
-    <div className=" flex pt-2 pr-4 pb-2 pl-4 items-center self-stretch rounded-[4px] border border-gray-60">
-      <input
-        className="w-[100%] outline-none"
-        onChange={handleChangeTitle}
-        placeholder="제목"
-      />
+    <div className=" flex pt-2 pr-4 pb-2 pl-4 items-center self-stretch rounded-[4px] border border-gray-60 bg-white">
+      <input className="w-[100%] outline-none" onChange={handleChangeTitle} placeholder="제목" />
     </div>
   );
 };
--- src/components/modifyActivity/ModifyPrice.tsx
+++ src/components/modifyActivity/ModifyPrice.tsx
@@ -34,7 +34,7 @@ const ModifyPrice = ({ price }: ModifyPriceProps) => {
   return (
     <div className=" flex w-[100%] flex-col items-start gap-4">
       <span className=" text-black text-2xl font-bold">가격</span>
-      <div className=" flex pt-2 pr-4 pb-2 pl-4 items-center self-stretch rounded-[4px] border border-gray-60">
+      <div className=" flex pt-2 pr-4 pb-2 pl-4 items-center self-stretch rounded-[4px] border border-gray-60 bg-white">
         <input
           type="number"
           className="w-[100%] outline-none"
