7373466eb9a9a9faa314cbf059ccf54f438e22ee
seoyeon <ysynop7@naver.com>
2024-06-12 20:22:34 +0900

    Design: 카드 호버 시 교체되는 색상 변경

--- src/components/mainpage/ActivityCard.tsx
+++ src/components/mainpage/ActivityCard.tsx
@@ -28,18 +28,18 @@ const ActivityCard = ({ cardData }: ActivityCardProps) => {
           className="w-[282px] h-[282px] bg-cover bg-center rounded-3xl group-hover:bg-extend md:w-[221px] md:h-[221px] sm:w-[168px] sm:h-[168px]"
           style={{ backgroundImage: `url(${bannerImageUrl})` }}
         />
-        <div className="flex flex-col gap-[10px] w-[282px] text-black leading-none group-hover:rounded-3xl group-hover:bg-blue-10 group-hover:text-blue-30 md:w-[221px] sm:w-[168px]">
+        <div className="flex flex-col gap-[10px] w-[282px] text-black leading-none px-2 py-2 group-hover:bg-blue-10 group-hover:rounded-2xl group-hover:text-blue-800 md:w-[221px] sm:w-[168px]">
           <div className="flex items-center gap-1">
             <img src="/assets/bold_star.svg" alt="little-star" />
-            <p className="font-medium sm:text-sm">
+            <p className="font-medium group-hover:text-blue-30 sm:text-sm">
               {rating}
-              <span className="text-gray-60">{` (${reviewCount})`}</span>
+              <span className="text-gray-60 group-hover:text-blue-800">{` (${reviewCount})`}</span>
             </p>
           </div>
           <div className="text-2xl font-semibold pb-[5px] sm:text-lg sm:leading-tight">{title}</div>
-          <div className="flex items-center gap-1 text-[28px] font-bold sm:text-xl sm:leading-none">
+          <div className="flex items-center gap-1 text-[28px] font-bold group-hover:text-blue-30 sm:text-xl sm:leading-none">
             {priceToWon(price)}
-            <span className="text-xl text-gray-80 font-normal group-hover:text-blue-20 sm:text-base">/인</span>
+            <span className="text-xl text-gray-80 font-normal group-hover:text-blue-800 sm:text-base">/인</span>
           </div>
         </div>
       </div>
