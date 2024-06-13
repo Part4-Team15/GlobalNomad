3d39ee7cb9b3b8ef73a2959b2bf8f1528fb6e390
seoyeon <ysynop7@naver.com>
2024-06-14 06:46:27 +0900

    Design: 모든 체험 스켈레톤 UI 반응형 디자인 수정

--- src/components/skeletonUI/mainpage/ActivityCardSkeleton.tsx
+++ src/components/skeletonUI/mainpage/ActivityCardSkeleton.tsx
@@ -9,13 +9,13 @@ const ActivityCardSkeleton = () => {
       <div className="w-[274px] flex flex-col gap-[10px] px-2 py-3 md:w-[221px] sm:w-[168px]">
         <div className="flex items-center gap-1">
           <img className="z-10" src="/assets/bold_star.svg" alt="little-star" />
-          <p className="w-8 h-4 bg-gray-300 rounded-xl sm:w-5 sm:h-[14px]" />
+          <p className="w-8 h-4 bg-gray-300 rounded-xl sm:w-7 sm:h-[14px]" />
         </div>
-        <div className="h-6 bg-gray-300 rounded-xl mb-[5px] sm:h-[18px]" />
+        <div className="w-[221px] h-6 bg-gray-300 rounded-xl mb-[5px] md:w-48 sm:w-36 sm:h-[18px]" />
         <div className="h-6 flex items-center gap-1 sm:h-5">
-          <div className="w-[130px] h-[28px] bg-gray-300 rounded-xl sm:h-5" />
+          <div className="w-[130px] h-[28px] bg-gray-300 rounded-xl md:w-[120px] sm:w-24 sm:h-5" />
           <span className="z-10">/</span>
-          <div className="w-10 h-[28px] bg-gray-300 rounded-xl sm:h-5" />
+          <div className="w-8 h-[28px] bg-gray-300 rounded-xl sm:w-5 sm:h-5 sm:rounded-lg" />
         </div>
       </div>
     </Skeleton>
