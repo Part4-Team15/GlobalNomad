0796fefddba936626fbf9c32ff1871bfd72e5a40
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2024-06-15 15:38:54 +0900

    style : 링크 페이지 컴포넌트 디자인  수정

--- src/components/signup/LinkToLoginPage.tsx
+++ src/components/signup/LinkToLoginPage.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 
 const LinkToLoginPage = () => (
-  <div className="flex mx-auto my-0 w-[640px] items-center justify-center gap-2 font-normal mt-8">
+  <div className="flex mx-auto my-0 w-[640px] items-center justify-center gap-2 font-normal mt-8 sm:w-[350px]">
     <div className="text-[#4B4B4B]">회원이신가요?</div>
     <Link to="/login" className="text-green-80 underline underline-offset-2">
       로그인하기
