661f2b3abcbad650e2e8da42da4cd5a18c95b49a
seoyeon <ysynop7@naver.com>
2024-06-18 15:08:51 +0900

    Refactor: 최근 방문한 체험 리스트 버튼 클릭시 보여질 수 있도록 수정

--- src/components/mainpage/RecentViewedActivity.tsx
+++ src/components/mainpage/RecentViewedActivity.tsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { CurrentViewedActivity, getRecentlyViewedActivities } from '@/utils/saveRecentActivities';
 import '../../styles/customScrollbar.css';
 
-const getRecentlyViewedActivities = () => {
-  const storedData = localStorage.getItem('recentlyViewedActivities');
-  return storedData ? JSON.parse(storedData) : [];
-};
-
 const RecentViewedActivity = () => {
+  const [showList, setShowList] = useState(false);
   const [recentViewedList, setRecentViewedList] = useState([]);
+
+  const handleClick = () => {
+    setShowList(!showList);
+  }
 
   useEffect(() => {
     const viewedList = getRecentlyViewedActivities();
@@ -16,25 +17,35 @@ const RecentViewedActivity = () => {
   }, []);
 
   return (
-    <div className="fixed top-4 right-4 rounded-2xl bg-white border border-green-80 p-4 z-10">
-      <div
-        className="w-36 h-48 flex flex-col items-center gap-3 overflow-y-scroll custom-scrollbar"
+    <>
+      <button
+        className="fixed top-1/2 -right-[2px]"
+        type='button'
+        onClick={handleClick}
       >
-        {recentViewedList.length ?
-          (recentViewedList.map((activity: any) => (
-            <Link key={activity.id} to={`/activity/${activity.id}`}>
-              <div
-                className="w-24 h-24 bg-cover bg-center rounded-3xl"
-                style={{ backgroundImage: `url(${activity.bannerImageUrl})` }}
-              />
-            </Link>
-          ))
-          ) : (
-            <div>최근에 본 체험 내역이 없습니다.</div>
-          )
-        }
-      </div>
-    </div>
+        <img src='/assets/pagination_arrow_left.svg' alt='show recent activity List'/>
+      </button>
+      {showList && <div className="fixed top-4 right-4 rounded-2xl bg-white border border-green-80 p-4 z-10">
+        <p className="text-lg text-center font-medium pb-2">최근 방문한 체험</p>
+        <div
+          className="w-36 h-48 flex flex-col items-center gap-3 overflow-y-scroll custom-scrollbar"
+        >
+          {recentViewedList.length ?
+            (recentViewedList.map((activity: CurrentViewedActivity) => (
+              <Link key={activity.id} to={`/activity/${activity.id}`}>
+                <div
+                  className="w-24 h-24 bg-cover bg-center rounded-3xl"
+                  style={{ backgroundImage: `url(${activity.bannerImageUrl})` }}
+                />
+              </Link>
+            ))
+            ) : (
+              <div>최근에 방문한 체험 내역이 없습니다.</div>
+            )
+          }
+        </div>
+      </div>}
+    </>
   );
 };
 
