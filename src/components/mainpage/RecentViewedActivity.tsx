b0f8efe25717a56886cf347883954101758ebad7
seoyeon <ysynop7@naver.com>
2024-06-18 14:02:55 +0900

    Refactor: 로직 이동 후 코드 정리

--- src/components/mainpage/RecentViewedActivity.tsx
+++ src/components/mainpage/RecentViewedActivity.tsx
@@ -2,42 +2,21 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../../styles/customScrollbar.css';
 
-const getCurrentViewedActivity = () => {
-  const storedData = localStorage.getItem('currentViewedActivity');
-  return storedData ? JSON.parse(storedData) : null;
-};
-
-// Local storage에 최근 본 게시물 목록을 저장하는 함수
-const setRecentlyViewedActivities = (activity: any) => {
-  localStorage.setItem('recentlyViewedActivities', JSON.stringify(activity));
-};
-
-
-// Local storage에서 최근 본 게시물 목록을 가져오는 함수
 const getRecentlyViewedActivities = () => {
   const storedData = localStorage.getItem('recentlyViewedActivities');
   return storedData ? JSON.parse(storedData) : [];
 };
 
-
 const RecentViewedActivity = () => {
   const [recentViewedList, setRecentViewedList] = useState([]);
 
   useEffect(() => {
-    const viewedActivity = getCurrentViewedActivity();
-
-    if (viewedActivity) {
-      let viewedList = getRecentlyViewedActivities();
-      viewedList = viewedList.filter((activity: any) => activity.id !== viewedActivity.id);
-      viewedList.unshift(viewedActivity);
-      setRecentViewedList(viewedList);
-      setRecentlyViewedActivities(viewedList);
-    }
+    const viewedList = getRecentlyViewedActivities();
+    setRecentViewedList(viewedList);
   }, []);
 
-
   return (
-    <div className="fixed top-4 right-4 rounded-2xl bg-white border border-green-80 p-4">
+    <div className="fixed top-4 right-4 rounded-2xl bg-white border border-green-80 p-4 z-10">
       <div
         className="w-36 h-48 flex flex-col items-center gap-3 overflow-y-scroll custom-scrollbar"
       >
