709c4accb072a7a8ead0d03739fee2f200aeed47
beom <gb3187@naver.com>
2024-06-18 22:15:11 +0900

    refactor : 카테고리 선택 div 선택시 드롭다운 활성화

--- src/components/assignActivity/AssignCategory.tsx
+++ src/components/assignActivity/AssignCategory.tsx
@@ -17,14 +17,17 @@ const AssignCategory = () => {
 
   return (
     <div className=" w-[100%] relative bg-white">
-      <div className=" flex pt-2 pr-4 pb-2 pl-4 items-center self-stretch rounded-[4px] border border-gray-60">
+      <div
+        className=" flex pt-2 pr-4 pb-2 pl-4 items-center self-stretch rounded-[4px] border border-gray-60"
+        onClick={handleDropDown}
+      >
         <input
           className="w-[100%] outline-none"
           value={selectedValue || ''}
           placeholder="카테고리"
           readOnly
         />
-        <button type="button" onClick={handleDropDown}>
+        <button type="button">
           <img
             src={isDropDown ? '/assets/arrow_up.svg' : '/assets/arrow_down.svg'}
             alt="arrowDownIcon"
--- src/components/modifyActivity/ModifyCategory.tsx
+++ src/components/modifyActivity/ModifyCategory.tsx
@@ -28,14 +28,17 @@ const ModifyCategory = ({ category }: ModifyCategoryProps) => {
 
   return (
     <div className=" w-[100%] relative">
-      <div className=" flex pt-2 pr-4 pb-2 pl-4 items-center self-stretch rounded-[4px] border border-gray-60 bg-white">
+      <div
+        className=" flex pt-2 pr-4 pb-2 pl-4 items-center self-stretch rounded-[4px] border border-gray-60 bg-white"
+        onClick={handleDropDown}
+      >
         <input
           className="w-[100%] outline-none"
           value={selectedValue || ''}
           placeholder="카테고리"
           readOnly
         />
-        <button type="button" onClick={handleDropDown}>
+        <button type="button">
           <img
             src={isDropDown ? '/assets/arrow_up.svg' : '/assets/arrow_down.svg'}
             alt="arrowDownIcon"
