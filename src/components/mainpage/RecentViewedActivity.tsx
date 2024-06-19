d33f3b275b5cf3b907242c5ff087d5c4cb209606
seoyeon <ysynop7@naver.com>
2024-06-19 18:09:07 +0900

    Refactor: 최근 방문한 체험 리스트 UI 수정 및 바깥 영역 클릭 시 리스트가 숨겨지도록 수정

--- src/components/mainpage/RecentViewedActivity.tsx
+++ src/components/mainpage/RecentViewedActivity.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from 'react';
+import { MouseEvent, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { CurrentViewedActivity, getRecentlyViewedActivities } from '@/utils/saveRecentActivities';
 import '../../styles/customScrollbar.css';
+import ModalPortal from '../review/ModalPortal';
 
 const RecentViewedActivity = () => {
   const [showList, setShowList] = useState(false);
@@ -9,6 +10,14 @@ const RecentViewedActivity = () => {
 
   const handleClick = () => {
     setShowList(!showList);
+  }
+
+  const handleOutsideClick = () => {
+    setShowList(false);
+  }
+
+  const handleNotClose = (e: MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
   }
 
   useEffect(() => {
@@ -19,32 +28,45 @@ const RecentViewedActivity = () => {
   return (
     <>
       <button
-        className="fixed top-1/2 -right-[2px]"
+        className="fixed top-1/4 -right-[2px] border border-green-10 rounded-l-md shadow-sm cursor-pointer"
         type='button'
         onClick={handleClick}
       >
-        <img src='/assets/pagination_arrow_left.svg' alt='show recent activity List'/>
+        <img
+          className="w-8 rounded-l-md"
+          src={showList ? '/assets/pagination_arrow_right.svg' : '/assets/pagination_arrow_left.svg'}
+          alt='show or hide recent activity List'
+        />
       </button>
-      {showList && <div className="fixed top-4 right-4 rounded-2xl bg-white border border-green-80 p-4 z-10">
-        <p className="text-lg text-center font-medium pb-2">최근 방문한 체험</p>
-        <div
-          className="w-36 h-48 flex flex-col items-center gap-3 overflow-y-scroll custom-scrollbar"
-        >
-          {recentViewedList.length ?
-            (recentViewedList.map((activity: CurrentViewedActivity) => (
-              <Link key={activity.id} to={`/activity/${activity.id}`}>
-                <div
-                  className="w-24 h-24 bg-cover bg-center rounded-3xl"
-                  style={{ backgroundImage: `url(${activity.bannerImageUrl})` }}
-                />
-              </Link>
-            ))
-            ) : (
-              <div>최근에 방문한 체험 내역이 없습니다.</div>
-            )
-          }
+      {showList && (
+      <ModalPortal>
+        <div className="fixed inset-0 z-10 bg-black bg-opacity-0" onClick={handleOutsideClick}>
+          <div onClick={handleNotClose}>
+            <div className="fixed top-1/4 right-12 rounded-2xl bg-white border border-green-700 shadow-green-10 shadow-custom px-4 pt-3 pb-4 z-10">
+              <p className="text-lg text-center font-medium pb-3">내가 최근 둘러본 체험</p>
+              <div
+                className="w-40 h-72 flex flex-col items-center gap-4 overflow-y-scroll custom-scrollbar pb-4"
+              >
+                {recentViewedList.length ?
+                  (recentViewedList.map((activity: CurrentViewedActivity) => (
+                    <Link key={activity.id} to={`/activity/${activity.id}`}>
+                      <div
+                        className="w-28 h-28 bg-cover bg-center rounded-3xl
+                          hover:border hover:border-green-300 hover:shadow-green-700 hover:shadow-custom"
+                        style={{ backgroundImage: `url(${activity.bannerImageUrl})` }}
+                      />
+                    </Link>
+                  ))
+                  ) : (
+                    <div>최근에 방문한 체험 내역이 없습니다.</div>
+                  )
+                }
+              </div>
+            </div>
+          </div>
         </div>
-      </div>}
+      </ModalPortal>
+      )}
     </>
   );
 };
