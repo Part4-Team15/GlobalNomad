4a8fce973c988bbd364633402be69d981f85d6a3
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2024-06-20 02:23:34 +0900

    style : transition 추가

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => (
-  <div className="flex flex-col w-full min-h-screen">
+  <div className="flex flex-col w-full min-h-screen [&_*]:transition [&_*]:duration-500">
     <Header />
     <div className="bg-gray-10 min-h-[calc(100vh-160px)]">
       <Outlet />
