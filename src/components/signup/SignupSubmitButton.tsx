a4486f5de8e55338ae07d4288fb6ee33fea153db
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2024-06-19 15:31:06 +0900

    refactor : 로그인, 회원가입 페이지 리팩터링

--- src/components/signup/SignupSubmitButton.tsx
+++ src/components/signup/SignupSubmitButton.tsx
@@ -7,7 +7,7 @@ const SignupSubmitButton = ({ children, disabled = false }: SignupSubmitButtonPr
     type="submit"
     disabled={disabled}
     className={`py-4 px-8 rounded-[6px] text-white text-lg font-semibold transition-opacity ${
-      disabled ? 'bg-gray-400' : 'bg-blue-500 hover:opacity-90'
+      disabled ? 'bg-gray-400' : 'bg-green-800 hover:opacity-90'
     }`}
   >
     {children}
