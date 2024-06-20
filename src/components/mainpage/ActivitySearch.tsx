59431584aed71ae2f50b806fa4d753c861b32deb
seoyeon <ysynop7@naver.com>
2024-06-21 00:48:26 +0900

    Design: 검색 창 darkmode 적용

--- src/components/mainpage/ActivitySearch.tsx
+++ src/components/mainpage/ActivitySearch.tsx
@@ -1,5 +1,6 @@
 import { FormEvent, useRef } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
+import { ReactComponent as SearchIcon } from './assets/search_icon.svg';
 
 const ActivitySearch = () => {
   const searchWord = useRef<HTMLInputElement>(null);
@@ -10,28 +11,27 @@ const ActivitySearch = () => {
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const inputValue = searchWord.current?.value;
-    if (inputValue === '') navigate('/');
     navigate(`/search?keyword=${inputValue || keyword}`);
   };
 
   return (
-    <div className="relative bg-white pb-[118px] sm:pb-[73px]">
+    <div className="relative bg-none pb-[118px] sm:pb-[73px]">
       <form
-        className="absolute -top-14 flex flex-col gap-8 px-6 py-8 shadow-custom rounded-2xl bg-white lg:w-[1000px] sm:gap-[15px] sm:py-4"
+        className="absolute -top-14 flex flex-col gap-8 px-6 py-8 shadow-custom rounded-2xl bg-white
+        dark:bg-darkMode-black-40 dark:shadow-darkMode-black-40 lg:w-[1000px] sm:gap-[15px] sm:py-4"
         onSubmit={handleSubmit}
       >
-        <label className="text-black text-xl font-bold sm:text-base">무엇을 체험하고 싶으신가요?</label>
+        <label className="text-black text-xl font-bold dark:text-darkMode-white-10 sm:text-base">무엇을 체험하고 싶으신가요?</label>
         <div className="flex items-center gap-3">
           <div className="group flex items-center w-[1004px] h-14 border border-gray-60 border-solid rounded-md
-          lg:w-[952px] md:w-[500px] sm:w-[187px] focus-within:border-green-40"
+          lg:w-[952px] md:w-[500px] sm:w-[187px] focus-within:border-green-40 focus-within:dark:border-darkMode-white-30"
           >
-            <img
-              className="w-6 h-6 m-3 md:m-2 sm:m-2"
-              src="/assets/search_icon.svg"
-              alt="search-icon"
-            />
+            <div className="w-6 h-6 m-3 md:m-2 sm:m-2">
+              <SearchIcon className="fill-green-80 dark:fill-darkMode-gray-10"/>
+            </div>
             <input
-              className="outline-none w-[930px] lg:w-[730px] md:w-[436px] sm:w-[124px]"
+              className="outline-none w-[930px] dark:bg-darkMode-black-40 dark:text-darkMode-white-10 
+              placeholder:dark:text-darkMode-white-10 lg:w-[730px] md:w-[436px] sm:w-[124px]"
               type="search"
               ref={searchWord}
               defaultValue={keyword || ''}
@@ -39,7 +39,8 @@ const ActivitySearch = () => {
             />
           </div>
           <button
-            className="bg-nomad-black rounded-md min-w-[136px] h-14 px-10 py-2 text-white font-bold sm:min-w-24 sm:px-5"
+            className="bg-nomad-black rounded-md min-w-[136px] h-14 px-10 py-2 text-white font-bold
+              dark:bg-darkMode-gray-10 dark:text-darkMode-black-40 sm:min-w-24 sm:px-5"
             type="submit"
           >
             검색하기
