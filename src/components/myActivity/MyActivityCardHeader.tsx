d5eefc4b3c728ec3ca86ba3678ef1fd70a65280b
Dodam0719 <blueoh719@gmail.com>
2024-06-22 13:34:12 +0900

    design : MyActivityCardHeader 다크모드 적용

--- src/components/myActivity/MyActivityCardHeader.tsx
+++ src/components/myActivity/MyActivityCardHeader.tsx
@@ -9,7 +9,9 @@ const MyActivityCardHeader = () => {
 
   return (
     <div className="w-full min-w-[21.5rem] flex justify-between mb-6">
-      <h2 className=" text-black font-bold text-[32px] self-start">내 체험 관리</h2>
+      <h2 className=" text-black font-bold text-[32px] self-start dark:text-darkMode-white-10">
+        내 체험 관리
+      </h2>
       <button
         type="button"
         className="flex min-w-[7.5rem] h-12 p-2.5 justify-center items-center gap-1 self-stretch rounded border-[1.5px] border-green-80 bg-[#121] text-white dark:bg-darkMode-black-20"
